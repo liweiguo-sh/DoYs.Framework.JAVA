db09c139bc5cfe44603797585435e9ea7d493402
liweiguo_sh <liweiguo_sh@outlook.com>
2020-09-25 12:22:21 +0800

    常规修改

--- src/main/java/com/doys/aprint/labels/maso/MasoFileController.java
+++ src/main/java/com/doys/aprint/labels/maso/MasoFileController.java
@@ -28,19 +28,21 @@ public class MasoFileController extends BaseController {
             dbBus.exec(sql, content, vars, labelId);
 
             // -- 1. 解析标签变量 -------------------------------
-            String[] arrVars = vars.split(Const.CHAR3);
-            for (int i = 0; i < arrVars.length; i++) {
-                String[] arrVar = arrVars[i].split(Const.CHAR4);
-                String name = arrVar[0];
-                String value = (arrVar.length == 2 ? arrVar[1] : "");
-                if (name.contains("标签元素") || name.contains("共享变量")) {
-                    continue;
-                }
+            if (vars.length() > 0) {
+                String[] arrVars = vars.split(Const.CHAR3);
+                for (int i = 0; i < arrVars.length; i++) {
+                    String[] arrVar = arrVars[i].split(Const.CHAR4);
+                    String name = arrVar[0];
+                    String value = (arrVar.length == 2 ? arrVar[1] : "");
+                    if (name.contains("标签元素") || name.contains("共享变量")) {
+                        continue;
+                    }
 
-                HashMap<String, Object> map = new HashMap<>();
-                map.put("name", name);
-                map.put("value", value);
-                listVars.add(map);
+                    HashMap<String, Object> map = new HashMap<>();
+                    map.put("name", name);
+                    map.put("value", value);
+                    listVars.add(map);
+                }
             }
 
             // -- 2. 保存标签变量 --
