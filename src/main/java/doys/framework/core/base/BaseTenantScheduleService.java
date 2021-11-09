b8f26dde282c08dc50af38a69f428135610b8d00
Volant Lee <volant@163.com>
2021-11-09 15:03:12 +0800

    2021-11-09

---
+++ src/main/java/doys/framework/core/base/BaseTenantScheduleService.java
@@ -0,0 +1,12 @@
+/******************************************************************************
+ * Copyright (C), 2020-2021, doys-next.com
+ * @author David.Li
+ * @version 1.0
+ * @create_date 2021-11-09
+ * @modify_date 2021-11-09
+ * 商户后台调度服务基类
+ *****************************************************************************/
+package doys.framework.core.base;
+public class BaseTenantScheduleService extends BaseTop {
+
+}
